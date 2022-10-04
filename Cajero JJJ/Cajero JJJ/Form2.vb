8e9bc0c8fb5d728283682f5a72318c6d06967ae0
Juanjo <juanjojaramillolop@gmail.com>
2022-10-04 18:57:51 -0500

    sam, descargate este archivo

---
+++ Cajero JJJ/Cajero JJJ/Form2.vb
@@ -0,0 +1,7 @@
+﻿Public Class Form2
+
+    Private Sub Button5_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button5.Click
+        Me.Hide()
+        Form1.Show()
+    End Sub
+End Class
