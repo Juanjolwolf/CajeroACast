480c3808a1422eac3cac89280b892338fd1c633d
Juanjo <juanjojaramillolop@gmail.com>
2022-10-04 22:59:24 -0500

    aqui estan todos los trabajos

---
+++ Cajero E Arenas/Cajero E Arenas/Form2.vb
@@ -0,0 +1,27 @@
+﻿Public Class Form2
+
+    Private Sub Button5_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button5.Click
+        Me.Hide()
+        Form1.Show()
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Me.Hide()
+        Form3.Show()
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        Me.Hide()
+        Form4.Show()
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        Me.Hide()
+        Form5.Show()
+    End Sub
+
+    Private Sub Button4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button4.Click
+        Me.Hide()
+        Form6.Show()
+    End Sub
+End Class
